721ca65da64e4faf5b95c36ab24c86f80b38f742
Elvira Neziri <neziiri.elvira@gmail.com>
2021-04-23 11:18:50 +0200

    Removed unnecessary console.log

--- src/controller.js
+++ src/controller.js
@@ -1,16 +1,13 @@
 const root = (req, res) => {
-    console.log(req.url);
     res.end('Home page is coming!');
 }
 
 const text = (req, res) => {
-    console.log(req.url);
     const param = req.params.param || 'Node';
     res.end(`Hello ${param} Server!`);
 }
 
 const html = (req, res) => {
-    console.log(req.url);
     const param = req.params.param || 'Node';
     res.end(`<h1>Hello ${param} Server!</h1>`);
 }
